52fb1bb6e316ae6ae6ec5beb0786e455f81191df
David Montesdeoca <backpackerhh@gmail.com>
2024-01-18 21:00:09 +0100

    Allow to display a default map in the page
    References:
    
    * https://developers.google.com/maps/documentation/javascript/overview
    * https://developers.google.com/maps/documentation/javascript/using-typescript
    * https://codereview.stackexchange.com/questions/237824/using-async-await-with-dynamic-import-for-es6-modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-console.log(google.maps);
+(async () => {
+  const { Map } = (await google.maps.importLibrary(
+    "maps"
+  )) as google.maps.MapsLibrary;
+  const mapDiv = document.getElementById("map") as HTMLElement;
+  const map = new Map(mapDiv, {
+    center: { lat: 27.92086, lng: -15.5706 }, // Gran Canaria, Canary Islands, Spain
+    zoom: 11,
+  }) as google.maps.Map;
+})();
